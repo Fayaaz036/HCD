f853e6a81e7d30615db9755e64100d16d3e90744
1190190 <fayaaz.nabie@hva.nl>
2024-04-17 13:58:58 +0200

    duidelijk stappenplan en werkend show hide

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ fetch('kleding.json')
             document.getElementById('output').classList.remove('hidden');
             
             outputDiv.innerHTML = `
-                <p>Je bent voor de casual stijl gegaan. Om te beginnen heb ik een <b>${willekeurigeObjectieveTrui}</b> gecombineerd met
-                 een leuke <b>${willekeurigeObjectieveBroek}</b>, en natuurlijk 
-                 hoort daar een schattige <b>${willekeurigObjectiefAccessoire}</b> bij! </p>
+                <p>Je bent voor de casual stijl gegaan. Om te beginnen heb ik een ${willekeurigeObjectieveTrui} gecombineerd met
+                 een leuke ${willekeurigeObjectieveBroek}, en natuurlijk 
+                 hoort daar een schattige ${willekeurigObjectiefAccessoire} bij! </p>
                 `
             })
 
@@ -40,9 +40,9 @@ fetch('kleding.json')
             document.getElementById('output').classList.remove('hidden');
 
             outputDiv.innerHTML =  `
-                <p>Je hebt voor een zakelijke outfit gekozen. Om te beginnen heb ik een <b>${willekeurigeSubjectieveTrui}</b> gecombineerd met
-                 een leuke <b>${willekeurigeSubjectieveBroek}</b>, en natuurlijk 
-                 hoort daar een schattige <b>${willekeurigSubjectieveAccessoire}</b> bij! </p>
+                <p>Je hebt voor een zakelijke outfit gekozen. Om te beginnen heb ik een ${willekeurigeSubjectieveTrui}</b> gecombineerd met
+                 een leuke ${willekeurigeSubjectieveBroek}, en natuurlijk 
+                 hoort daar een schattige ${willekeurigSubjectieveAccessoire} bij! </p>
                 `
         })
 
@@ -65,14 +65,17 @@ fetch('kleding.json')
 
 
 document.getElementById('addOutfit').addEventListener('click', function() {
+    const selectedValue = document.querySelector('input[name="keuze"]:checked');
     const text = document.getElementById('kledingstuk').value;
+    const message = document.getElementById('message');
 
-    // Maak een AJAX-verzoek naar een server-side script om de tekst naar de JSON-bestand te sturen
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'saveData.php'); // Vervang 'saveData.php' door de naam van je server-side script
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({ text: text }));
+    if (!selectedValue || !text) {
+        message.innerText = 'Selecteer een categorie en voer een kledingstuk in.';
+    } else {
+        message.innerText = `Gelukt! Ik heb ${text} toegevoegd aan de categorie ${selectedValue.value}.`;
+    }
 });
+
 
 
 
